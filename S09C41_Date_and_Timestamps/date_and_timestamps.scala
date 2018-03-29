74273c07e3de2c9ea46f5601b5da110c599ed3d1
Patrick Alex Freitas da Silva <patrickalex@gmail.com>
2018-03-29 14:53:18 -0300

    Adds S09C41_Date_and_Timestamp/date_and_timestamp

--- S09C41_Date_and_Timestamps/date_and_timestamps.scala
+++ S09C41_Date_and_Timestamps/date_and_timestamps.scala
@@ -1,7 +1,24 @@
 import org.apache.spark.sql.SparkSession
 val spark = SparkSession.builder().getOrCreate()
 
-val df = spark.read.option("header","true").option("inferSchema","true").csv("../S09C37_Data_Frames/Spark DataFrames/CitiGroup2006_2008")
+val df = spark.read.option("header","true").option("inferSchema","true").csv("./S09C37_Data_Frames/Spark DataFrames/CitiGroup2006_2008")
 
 df.printSchema()
 df.show()
+
+println("Getting one timestamp column")
+df.select(year(df("Date"))).show()
+
+val df2 = df.withColumn("Year", year(df("Date")))
+
+val dfavgs = df2.groupBy("Year").mean()
+dfavgs.select($"Year", $"avg(Close)").show()
+
+val dfstddev = df2.groupBy("Year").agg(stddev($"Close"))
+dfstddev.select($"Year", $"stddev_samp(Close)").show()
+
+val dfmax = df2.groupBy("Year").max()
+dfmax.select($"Year", $"max(Close)").show()
+
+val dfmin = df2.groupBy("Year").min()
+dfmin.select($"Year", $"min(Close)").show()
