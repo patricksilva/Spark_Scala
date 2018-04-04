6c02d598184a2f07fd3328caa4a63e202c5da1ff
Patrick Alex Freitas da Silva <patrickalex@gmail.com>
2018-04-04 13:17:42 -0300

    Adds Clean-Ecommerce.csv file and edit .scala script to start spark session, load data and print one line

--- S11C54_Linear_Regression_Exercise_Project/LinRegExercise.scala
+++ S11C54_Linear_Regression_Exercise_Project/LinRegExercise.scala
@@ -4,6 +4,8 @@
 /////////////////////////////////////////
 
 // Import LinearRegression
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.regression.LinearRegression
 
 // Optional: Use the following code below to set the Error reporting
 import org.apache.log4j._
@@ -11,16 +13,29 @@ Logger.getLogger("org").setLevel(Level.ERROR)
 
 
 // Start a simple Spark Session
+val spark = SparkSession.builder().getOrCreate()
 
 // Use Spark to read in the Ecommerce Customers csv file.
+val data = (spark.read
+    .option("header","true")
+    .option("inferSchema","true")
+    .format("csv")
+    .load("Clean-Ecommerce.csv")
+  )
 
 // Print the Schema of the DataFrame
-
+data.printSchema
 
 // Print out an example Row
 // Various ways to do this, just
 // choose whichever way you prefer
-
+val colnames = data.columns
+val firstrow = data.head(1)(0)
+println("\n")
+println("Example Data Row:")
+for(ind <- Range(1, colnames.length)){
+  println(colnames(ind) + ": " + firstrow(ind))
+}
 
 ////////////////////////////////////////////////////
 //// Setting Up DataFrame for Machine Learning ////
