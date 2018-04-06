b12221596965b0a02d0a8e278391d5b0bb06064a
Patrick Alex Freitas da Silva <patrickalex@gmail.com>
2018-04-06 01:13:25 -0300

    Adds preparing a data frame appropriate for machine learning and dropping missing values sections

--- S12C58_Spark_Classification_Logistic_Regression_Example/logregex.scala
+++ S12C58_Spark_Classification_Logistic_Regression_Example/logregex.scala
@@ -17,3 +17,31 @@ val data_frame = (spark.read
     .load("titanic.csv")
   )
 
+// Print out the schema (SisSp: "Siblings and Spouses"; Parch: "Parents and Children")
+data_frame.printSchema()
+
+// Print one line using field delimitator " and field separator ,
+val first_row = data_frame.head(1)(0)
+data_frame.columns.foreach(x => print("$\""+x + "\", "))
+println()
+for(i <- Range(0,first_row.length)){
+  print("\""+first_row(i) + "\",\t")
+}
+
+
+// Preparing a data frame appropriate for machine learning
+val logregdataall = (data_frame
+    .select(data_frame("Survived").as("label")
+    , $"Pclass", $"Name", $"Sex", $"Age", $"SibSp", $"Parch"
+    //  , $"Ticket"
+    , $"Fare"
+    //, $"Cabin"
+    , $"Embarked"
+    )
+  )
+
+
+// Dropping Mising Values
+val logregdata = logregdataall.na.drop()
+
+
