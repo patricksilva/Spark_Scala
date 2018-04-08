c41691236058df52af5fe67fa1e842634f87e937
Patrick Alex Freitas da Silva <patrickalex@gmail.com>
2018-04-08 13:51:16 -0300

    Adds S12C60 Classification Project Logistic Regression Answers

--- S12C60_Classification_Project_Logistic_Regression_Exercise/LogRegProject.scala
+++ S12C60_Classification_Project_Logistic_Regression_Exercise/LogRegProject.scala
@@ -26,21 +26,29 @@
 //////////////////////
 
 // Import SparkSession and Logistic Regression
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.classification.LogisticRegression
 
 // Optional: Use the following code below to set the Error reporting
+import org.apache.log4j._
+Logger.getLogger("org").setLevel(Level.ERROR)
 
 // Create a Spark Session
+val spark = SparkSession.builder().getOrCreate()
 
 // Use Spark to read in the Advertising csv file.
+val df = spark.read.option("header","true").option("inferSchema","true").format("csv").load("advertising.csv")
 
 // Print the Schema of the DataFrame
-
+df.printSchema
 ///////////////////////
 /// Display Data /////
 /////////////////////
 
 // Print out a sample row of the data (multiple ways to do this)
-
+val colunmNames = df.columns
+colunmNames.foreach(x => print(x + ","))
+df.head(3).foreach(x => println(x))
 
 ////////////////////////////////////////////////////
 //// Setting Up DataFrame for Machine Learning ////
@@ -50,40 +58,69 @@
 //    - Rename the Clicked on Ad column to "label"
 //    - Grab the following columns "Daily Time Spent on Site","Age","Area Income","Daily Internet Usage","Timestamp","Male"
 //    - Create a new column called Hour from the Timestamp containing the Hour of the click
-
-
+val df2  = df.withColumn("Hour", hour(df("Timestamp")))
+df2.printSchema
+val df_for_ml = (df2.select(df2("Clicked on Ad").as("label")
+  ,$"Daily Time Spent on Site"
+  ,$"Age"
+  ,$"Area Income"
+  ,$"Daily Internet Usage"
+  //,$"Timestamp"
+  ,$"Male"
+  ,$"Hour")
+  )
+df_for_ml.printSchema
 
 // Import VectorAssembler and Vectors
+import org.apache.spark.ml.feature.{VectorAssembler}
+import org.apache.spark.ml.linalg.Vectors
 
 // Create a new VectorAssembler object called assembler for the feature
 // columns as the input Set the output column to be called features
+val assembler = (new VectorAssembler()
+    .setInputCols(
+        Array("Daily Time Spent on Site", "Age", "Area Income", "Daily Internet Usage"
+      //, "Timestamp"
+      , "Male", "Hour")
+    )
+    .setOutputCol("features")
+  )
 
 
 // Use randomSplit to create a train test split of 70/30
-
+val Array(training, test) = df_for_ml.randomSplit(Array(0.7, 0.3), seed=12345)
 
 ///////////////////////////////
 // Set Up the Pipeline ///////
 /////////////////////////////
 
 // Import Pipeline
+import org.apache.spark.ml.Pipeline
+
 // Create a new LogisticRegression object called lr
+val lr = new LogisticRegression()
 
 // Create a new pipeline with the stages: assembler, lr
+val pipeline = new Pipeline().setStages(Array(assembler, lr))
 
 // Fit the pipeline to training set.
-
+val model = pipeline.fit(training)
 
 // Get Results on Test Set with transform
+val results = model.transform(test)
 
 ////////////////////////////////////
 //// MODEL EVALUATION /////////////
 //////////////////////////////////
 
 // For Metrics and Evaluation import MulticlassMetrics
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
 
 // Convert the test results to an RDD using .as and .rdd
+val predictionAndLabels = results.select($"prediction",$"label").as[(Double, Double)].rdd
 
 // Instantiate a new MulticlassMetrics object
+val metrics = new MulticlassMetrics(predictionAndLabels)
 
 // Print out the Confusion matrix
+metrics.confusionMatrix
